7690f18be5fd24a8754fab15256999c501e8d72d
ricardodev04 <rapineda@innovacion.gob.sv>
2025-04-24 09:13:45 -0600

    6.3 - 6.8

---
+++ redux-anecdotes/src/App.jsx
@@ -0,0 +1,13 @@
+import NewAnecdote from './components/NewAnecdote'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <AnecdoteList />
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
